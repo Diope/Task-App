1ba5fb2c6d23d1602ba2d04ae1185d21bacbb1be
Dion <kalani.pettaway@gmail.com>
2015-01-28 19:00:15 -0500

    Added user id to tasks, configured user task associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :tasks, dependent: :destroy
+  #Note: 'dependent: :destroy' makes sure when the user cancels their account
+  # all associated information (in this case tasks) also get destroyed
+
 end
